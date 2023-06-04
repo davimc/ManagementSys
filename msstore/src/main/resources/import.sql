14a02e4e785e0f0c4e341d9954e6c78eb7ce3a30
davimc <davimatosc@hotmail.com>
2023-06-04 18:22:33 -0300

    seed

---
+++ msstore/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO store(name) VALUES ('First Store');
+INSERT INTO store(name) VALUES ('Chicago');
+INSERT INTO store(name) VALUES ('Buenos Aires');
+INSERT INTO store(name) VALUES ('Amsterdã');
+INSERT INTO store(name) VALUES ('Estônia');
