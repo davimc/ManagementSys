e1ef5f3bdf6ff0e57ec6d854088d0dbf52e52dae
davimc <davimatosc@hotmail.com>
2023-06-03 23:48:37 -0300

    Create ms store

---
+++ msstore/src/test/java/io/github/davimc/msstore/MsstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.davimc.msstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
