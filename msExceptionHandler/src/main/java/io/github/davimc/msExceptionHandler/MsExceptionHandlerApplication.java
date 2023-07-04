5f0348193632c40a4fc796d45836d9d7aba5bd90
davimc <davimatosc@hotmail.com>
2023-07-04 11:39:39 -0300

    Standardizing exceptions

---
+++ msExceptionHandler/src/main/java/io/github/davimc/msExceptionHandler/MsExceptionHandlerApplication.java
@@ -0,0 +1,13 @@
+package io.github.davimc.msExceptionHandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsExceptionHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsExceptionHandlerApplication.class, args);
+	}
+
+}
