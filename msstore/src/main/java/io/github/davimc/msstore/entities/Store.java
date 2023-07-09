716336556c94aa5151508c18b15a1443eb8c0a74
davimc <davimatosc@hotmail.com>
2023-07-09 11:25:15 -0300

    update: cnpj unique

--- msstore/src/main/java/io/github/davimc/msstore/entities/Store.java
+++ msstore/src/main/java/io/github/davimc/msstore/entities/Store.java
@@ -1,19 +1,18 @@
 package io.github.davimc.msstore.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.Objects;
 
 @Entity
+@Table(name = "tb_store")
 public class Store {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
+    @Column(unique = true)
     private String cnpj;
 
     public Store() {
--- msstore/src/main/resources/import.sql
+++ msstore/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO store(name) VALUES ('First Store');
-INSERT INTO store(name) VALUES ('Chicago');
-INSERT INTO store(name) VALUES ('Buenos Aires');
-INSERT INTO store(name) VALUES ('Amsterdã');
-INSERT INTO store(name) VALUES ('Estônia');
+INSERT INTO tb_store(name, cnpj) VALUES ('First Store', '04.723.608/0001-03');
+INSERT INTO tb_store(name, cnpj) VALUES ('Chicago','23.706.491/0001-96');
+INSERT INTO tb_store(name, cnpj) VALUES ('Buenos Aires','07.981.740/0001-03');
+INSERT INTO tb_store(name, cnpj) VALUES ('Amsterdã', '42.645.343/0001-53');
+INSERT INTO tb_store(name, cnpj) VALUES ('Estônia', '38.158.616/0001-68');
