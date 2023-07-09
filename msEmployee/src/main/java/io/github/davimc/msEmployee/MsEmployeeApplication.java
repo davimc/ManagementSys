99865bfcd4c223369f4ffe95686e47ebccd53ca5
davimc <davimatosc@hotmail.com>
2023-07-09 11:23:26 -0300

    Separating profiles

--- msEmployee/src/main/java/io/github/davimc/msEmployee/MsEmployeeApplication.java
+++ msEmployee/src/main/java/io/github/davimc/msEmployee/MsEmployeeApplication.java
@@ -1,15 +1,28 @@
 package io.github.davimc.msEmployee;
 
+import io.github.davimc.msEmployee.config.YAMLConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class MsEmployeeApplication {
+public class MsEmployeeApplication implements CommandLineRunner {
+
+	@Autowired
+	private YAMLConfig myConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsEmployeeApplication.class, args);
+	}
+
+	public void run(String... args) {
+		System.out.println("using environment: " + myConfig.getEnvironment());
+		System.out.println("name: " + myConfig.getName());
+		System.out.println("enabled:" + myConfig.isEnabled());
+		System.out.println("servers: " + myConfig.getServers());
 	}
 
 }
--- msEmployee/src/main/resources/import.sql
+++ msEmployee/src/main/resources/import.sql
@@ -1,1 +1,7 @@
-INSERT INTO tb_employee(id,name, cpf, email, pix, birthdate, resignation_date, salary) VALUES ('458e8557-014f-4806-8f45-258d487569db','Huguinho', '12345678901','huguinho@gmail.com','12345678901', '1999-08-19', null, 3800.00)
+INSERT INTO tb_store_employee(name, cnpj) VALUES ('First Store', '04.723.608/0001-03');
+INSERT INTO tb_store_employee(name, cnpj) VALUES ('Chicago','23.706.491/0001-96');
+INSERT INTO tb_store_employee(name, cnpj) VALUES ('Buenos Aires','91.087.067/0001-60');
+INSERT INTO tb_store_employee(name, cnpj) VALUES ('Amsterdã', '42.645.343/0001-53');
+INSERT INTO tb_store_employee(name, cnpj) VALUES ('Estônia', '38.158.616/0001-68');
+
+INSERT INTO tb_employee(id,name, cpf, email, pix, birthdate, admission_date, salary,store_id) VALUES ('699e20fe-1e5c-11ee-be56-0242ac120002', 'Huguinho', '12345678901','huguinho@gmail.com','12345678901', '1999-08-19', '2003-12-31', 3800.00,1)
