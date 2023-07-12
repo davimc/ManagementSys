6454b2bee4d258ba43838bd7773525353406759f
davimc <davimatosc@hotmail.com>
2023-07-12 10:24:28 -0300

    msexchange

---
+++ msexchange/src/test/java/io/github/davimc/msexchange/MsexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.davimc.msexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsexchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
