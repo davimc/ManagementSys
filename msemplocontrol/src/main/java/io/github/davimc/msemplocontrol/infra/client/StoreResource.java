c0de1af56b60e9a67baeea85545a2cfb601ed16a
davimc <davimatosc@hotmail.com>
2023-07-14 11:11:58 -0300

    fix

---
+++ msemplocontrol/src/main/java/io/github/davimc/msemplocontrol/infra/client/StoreResource.java
@@ -0,0 +1,13 @@
+package io.github.davimc.msemplocontrol.infra.client;
+
+import io.github.davimc.msemplocontrol.model.DataEmployee;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "msstore", path = "/stores")
+public interface StoreResource {
+    @GetMapping(params = "cnpj")
+    ResponseEntity<DataEmployee> findByCnpj(@RequestParam String cpf);
+}
