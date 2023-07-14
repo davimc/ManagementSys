bd51fa36aef2d497c338b85b30b110272fbd60af
davimc <davimatosc@hotmail.com>
2023-07-14 10:07:20 -0300

    Identifier

--- msExceptionHandler/src/main/java/io/github/davimc/msExceptionHandler/exceptions/ObjectNotFoundException.java
+++ msExceptionHandler/src/main/java/io/github/davimc/msExceptionHandler/exceptions/ObjectNotFoundException.java
@@ -5,10 +5,7 @@ import java.util.UUID;
 
 //TODO criar ms para controlar exceptions e tratamento de erros
 public class ObjectNotFoundException extends RuntimeException {
-    public ObjectNotFoundException(Long id, Class<?> className) {
-        super("Object not found. id: " + id + " to: " + className.getSimpleName());
-    }
-    public ObjectNotFoundException(UUID id, Class<?> className) {
-        super("Object not found. id: " + id + " to: " + className.getSimpleName());
+    public ObjectNotFoundException(String id, Class<?> className) {
+        super("Object not found. identifier: " + id + " to: " + className.getSimpleName());
     }
 }
