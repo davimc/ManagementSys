ad573abb4a649485eb0733ebfff7d200ebd8c1ce
davimc <davimatosc@hotmail.com>
2023-07-16 10:26:59 -0300

    Feign store data

--- msemplocontrol/src/main/java/io/github/davimc/msemplocontrol/controllers/EmploControlController.java
+++ msemplocontrol/src/main/java/io/github/davimc/msemplocontrol/controllers/EmploControlController.java
@@ -1,7 +1,9 @@
 package io.github.davimc.msemplocontrol.controllers;
 
 import io.github.davimc.msemplocontrol.infra.client.EmployeeResource;
+import io.github.davimc.msemplocontrol.infra.client.StoreResource;
 import io.github.davimc.msemplocontrol.model.DataEmployee;
+import io.github.davimc.msemplocontrol.model.DataStore;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class EmploControlController {
     private final EmployeeResource employeeResource;
+    private final StoreResource storeResource;
     @GetMapping
     public String status() {
         return "Ok";
@@ -23,5 +26,10 @@ public class EmploControlController {
     public ResponseEntity<DataEmployee> findEmployee(@RequestParam String cpf) {
         return (employeeResource.findByCpf(cpf));
     }
+    @GetMapping(params = "cnpj")
+    public ResponseEntity<DataStore> findStore(@RequestParam String cnpj) {
+        return (storeResource.findByCnpj(cnpj));
+    }
+
 
 }
--- msemplocontrol/src/main/java/io/github/davimc/msemplocontrol/infra/client/StoreResource.java
+++ msemplocontrol/src/main/java/io/github/davimc/msemplocontrol/infra/client/StoreResource.java
@@ -1,6 +1,7 @@
 package io.github.davimc.msemplocontrol.infra.client;
 
 import io.github.davimc.msemplocontrol.model.DataEmployee;
+import io.github.davimc.msemplocontrol.model.DataStore;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,5 +10,5 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(value = "msstore", path = "/stores")
 public interface StoreResource {
     @GetMapping(params = "cnpj")
-    ResponseEntity<DataEmployee> findByCnpj(@RequestParam String cpf);
+    ResponseEntity<DataStore> findByCnpj(@RequestParam String cnpj);
 }
