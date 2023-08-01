67e532bcff29c950dfd2a7e0acc7f588226c5b6f
davimc <davimatosc@hotmail.com>
2023-08-01 16:52:59 -0300

    Configurating Security

---
+++ eurekaserver/src/main/java/io/github/davimc/eurekaserver/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package io.github.davimc.eurekaserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends SecurityConfigurerAdapter {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
+}
